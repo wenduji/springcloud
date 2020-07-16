0d51e271b0d67c25e1aa1bc4cb6246a3c7bbec52
jshe <2285563864@qq.com>
2020-07-16 17:18:56 +0800

    ThreadPoolExecutor

--- src/test/java/com/example/demo/stream/StreamTest.java
+++ src/test/java/com/example/demo/stream/StreamTest.java
@@ -7,14 +7,15 @@ import java.util.stream.IntStream;
 
 public class StreamTest {
     @Test
-    public void test1(){
-        int[] sourceArr = {12,33,10,99,11,12};
+    public void test1() {
+        int[] sourceArr = {12, 33, 10, 99, 11, 12};
         int[] desArr = IntStream.of(sourceArr)
-                .filter((element)->element>10)
-                .map((element)->element+1)
+                .filter(element -> element > 10)
+                .map(element -> element + 1)
                 .distinct()
                 .sorted()
                 .toArray();
-        System.out.println(Arrays.asList(desArr).toString());
+        IntStream.of(desArr)
+                .forEach(element -> System.out.println(element));
     }
 }
---
+++ src/test/java/com/example/demo/thread_pool/ThreadPoolTest.java
@@ -0,0 +1,32 @@
+package com.example.demo.thread_pool;
+
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.extension.ExtendWith;
+import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.mock.web.MockHttpServletRequest;
+import org.springframework.test.context.junit.jupiter.SpringExtension;
+import org.springframework.test.web.servlet.MockMvc;
+import org.springframework.test.web.servlet.request.MockMvcRequestBuilders;
+
+import javax.annotation.Resource;
+
+/**
+ * @author hjs
+ * @date 2020/7/16
+ * @description
+ */
+@SpringBootTest
+@ExtendWith(SpringExtension.class)
+@AutoConfigureMockMvc
+public class ThreadPoolTest {
+    @Resource
+    private MockMvc mockMvc;
+
+    @Test
+    public void threadPool() throws Exception {
+        mockMvc.perform(
+                MockMvcRequestBuilders.get("/rest/pools")
+        );
+    }
+}
