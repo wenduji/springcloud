7f11b39c3331e1956f87a8f14c8d7c9b32e9f83d
jshe <2285563864@qq.com>
2020-07-22 09:50:37 +0800

    删除被抽成模块的原先代码

--- eureka/src/test/java/com/example/eureka/EurekaApplicationTests.java
+++ eureka/src/test/java/com/example/eureka/EurekaApplicationTests.java
@@ -1,14 +1,13 @@
-package com.example.demo;
+package com.example.eureka;
 
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 
 @SpringBootTest
-class DemoApplicationTests {
+class EurekaApplicationTests {
 
     @Test
     void contextLoads() {
-
     }
 
 }
